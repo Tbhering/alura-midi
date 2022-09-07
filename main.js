d8a9ac3d461542de26f7f0d6c14619ee435d2673
Thalles Bhering <thbg1@hotmail.com>
2022-09-07 08:44:31 -0300

    Melhorando código, passando while para for

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 function tocasom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
-}
-
+};
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-//enquanto=while
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumentos = tecla.classList[1];
-    //template string
-    const idAudio = `#som_${instrumentos}`;
+    const idAudio = `#som_${instrumentos}`; //template string
     console.log(idAudio)
 
     tecla.onclick = function () {
         tocasom(idAudio);
     }
+};
+
+/*while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumentos = tecla.classList[1];
+    const idAudio = `#som_${instrumentos}`; //template string
+    console.log(idAudio)
+    tecla.onclick = function () {
+        tocasom(idAudio);}
     contador = contador + 1;
 
     console.log(contador)
-};
+};*/
